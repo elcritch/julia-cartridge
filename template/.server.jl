1ea49c47c526677bdba7771bf5777ff9355d8cc9
NEOMATRIX <josuecevedo@hotmail.com>
2016-01-16 00:11:17 -0600

    Actualizando a julia 0.4.2

--- template/.server.jl
+++ template/.server.jl
@@ -1,12 +1,13 @@
 using HttpServer
-using Meddle
+using Mux
 using HttpCommon
-using WebSockets
+#using WebSockets
 import JSON
-include(".mandelbrot.jl");
+using Mongo, LibBSON
 
 
-#################### WEBSERVER ####################
+client = MongoClient("mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/")
+
 function error_msg(status)
   msg = STATUS_CODES[status]
   return "
@@ -18,92 +19,35 @@ function error_msg(status)
 "
 end
 
-
-NotFoundLayer = Midware() do req::MeddleRequest, res::Response
-  respond(req, Response(404, error_msg(404)))
+function serve_file(req, res)
+    # Serve file as is.
+    # Permission to access the file is determined by middleware.
+    ur= ENV["OPENSHIFT_REPO_DIR"]
+    filename    = string(ur, req.resource)    # Example: "/static/js/bootstrap.min.js" becomes "../../static/js/bootstrap.min.js"
+    res2        = FileResponse(filename)           # A Response object containing the file in its data field
+    res.status  = res2.status
+    res.headers = res2.headers
+    res.data    = res2.data
 end
 
 
-# Deny files beginning with dot (better protect it with constant definition when possible)
-function DenyFilesLayer(root)
-  Midware() do req::MeddleRequest, res::Response
-    resource = normpath(req.state[:resource])
-    if ismatch(r"^/*.*(/\..+)$", resource)
-      return respond(req, Response(403, error_msg(403)))
-    end
-    req, res
-  end
-end
+@app test = (
+         Mux.defaults,
+         page(respond("<h1>Hello World!</h1>")),
+         page("/about",
+              probabilty(0.1, respond("<h1>Boo!</h1>")),
+              respond("<h1>About Me</h1>")),
+         page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
+    
+        post("/hellos", req -> create(Hellos_Controller(), req)),
+        put("/hellos/:id",req -> update(Hellos_Controller(), req)),
+        delete("/hellos/:id", req -> destroy(Hellos_Controller(), req)),
+        Mux.notfound())
 
-
-# Routes (upgrade to Morsel)
-ProcessFileLayer = Midware() do req::MeddleRequest, res::Response
-  resource = normpath(req.state[:resource])
-  if ismatch(r"^/$", resource)
-    req.state[:resource] = "/index.html"
-  elseif ismatch(r"^/about$", resource)
-    return respond(req, Response(200, "About dynamic content"))
-  end
-  req, res
-end
-
-
-function get_stack()
-  middleware(
-    DefaultHeaders,
-    URLDecoder,
-    DenyFilesLayer(ENV["OPENSHIFT_REPO_DIR"]),
-    CookieDecoder,
-    BodyDecoder,
-    ProcessFileLayer,
-    FileServer(ENV["OPENSHIFT_REPO_DIR"]),
-    NotFoundLayer)
-end
-
-
-http = HttpHandler() do req::Request, res::Response
-  mreq = MeddleRequest(req, Dict{Symbol,Any}(), Dict{Symbol,Any}())
-  Meddle.handle(get_stack(), mreq, res)
-end
-
-#################### WEBSOCKET ####################
-function write(ws::WebSocket, data::Array{Uint8})
-  if ws.is_closed
-    @show ws
-    error("attempt to write to closed WebSocket\n")
-  end
-  WebSockets.send_fragment(ws, true, data, 0b0010)
-end
-
-
-ws = WebSocketHandler() do req, client
-  while true
-    message = utf8(read(client))
-    args = JSON.parse(message)
-    ecuations = [
-      -0.8+0.16im,
-      -0.4+0.6im,
-      0.285+0im,
-      0.285+0.01im,
-      0.45+0.1428im,
-      -0.70176-0.3842im,
-      -0.835-0.2321im,
-      -0.8+0.156im,
-      -0.74543+0.11301im,
-      -0.1+0.651im
-    ]
-    c0 = ecuations[args["num"]]
-    mandelbrot = Uint8[]
-    w = int(args["width"])
-    h = int(args["height"])
-    for y=1:h, x=1:w
-      c = complex((x-w/2)/(w/2), (y-h/2)/(w/2))
-      push!(mandelbrot, juliaset(c, c0, 255))
-    end
-    write(client, mandelbrot)
-  end
-end
 
 host = getaddrinfo(ENV["OPENSHIFT_JULIA_IP"])
 port = int(ENV["OPENSHIFT_JULIA_PORT"])
-run(Server(http, ws), host=host, port=port)
+
+#serve(test,8080)
+
+run(Server(Mux.http_handler(app)), port)
