65b8b47df953125da8fb0d9aee4443ec9195dad5
neomatrix <josuecevedo@gmail.com>
2016-01-26 18:02:26 -0600

    Update server.jl

--- template/server.jl
+++ template/server.jl
@@ -7,5 +7,12 @@ port = parse(Int,ENV["OPENSHIFT_JULIA_PORT"])
 
   server = Merly.app(ur,"jl")
   @page "/" File("welcome.html", res)
+  server.notfound("
+    <!DOCTYPE html>
+    <html>
+    <head><title>Not found</title></head>
+    <body><h1>404, Not found</h1></body>
+    </html>
+  ")
 
   server.start("$host", port)
